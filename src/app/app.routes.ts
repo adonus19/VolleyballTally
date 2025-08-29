26dbacc5836f55512ba9c6ccff029c57c00791b4
Daniel Pogue <daniel.pogue@aig.com>
2025-08-29 13:58:44 -0400

    updated routes, nested pages into views folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'sessions', pathMatch: 'full' },
+  { path: 'sessions', loadComponent: () => import('./views/sessions/sessions').then(m => m.Sessions) },
+  { path: 'sessions/:id', loadComponent: () => import('./views/session-detail/session-detail').then(m => m.SessionDetail) },
+  { path: 'roster', loadComponent: () => import('./views/roster/roster').then(m => m.Roster) },
+  { path: 'leaderboard', loadComponent: () => import('./views/leaderboard/leaderboard').then(m => m.Leaderboard) },
+  { path: 'settings', loadComponent: () => import('./views/settings/settings').then(m => m.Settings) },
+];
