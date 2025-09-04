2a7a97afdc61c56afc169388b4deaa8b160107aa
Daniel Pogue <daniel.pogue@aig.com>
2025-09-04 12:24:23 -0400

    Fixed positioning for totals

--- src/app/views/leaderboard/leaderboard.ts
+++ src/app/views/leaderboard/leaderboard.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIcon } from "@angular/material/icon";
+import { MatChipsModule } from '@angular/material/chips';
+
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [],
+  imports: [MatListModule, MatIcon, MatChipsModule],
   templateUrl: './leaderboard.html',
   styleUrl: './leaderboard.scss'
 })
 export class Leaderboard {
+  private data = inject(DataService);
+  rows = computed(() => this.data.getTotals());
 
+  nameFor(pid: string) {
+    return this.data.players().find(p => p.id === pid)?.name || '(Removed)';
+  }
 }
